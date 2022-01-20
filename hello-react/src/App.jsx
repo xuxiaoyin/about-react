13974ca65f5d8d7ce4c34ce19136f538b53a6769
xiaoyin.xu@vcarsystem <1234567890>
2022-01-20 14:27:31 +0800

    代理的第一种方式，配置package.json

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,71 +1,29 @@
-import React, { Component } from 'react'
-import Add from './components/Add'
-import List from './components/List'
-import Footer from './components/Footer'
-import './app.css'
+import React, {Component} from 'react';
+import axios from 'axios'
 
-export default class App extends Component{
-  state = {
-    todos: [
-      {id: '001', name: '抽烟', done: true},
-      {id: '002', name: '喝酒', done: false},
-      {id: '003', name: '烫头', done: true},
-      {id: '004', name: '打豆豆', done: false}
-    ]
-  }
-
-  // 添加todo
-  addTodo = (todoObj) => {
-    const { todos } = this.state
-    this.setState({todos: [...todos, todoObj]})
-  }
-
-  // 删除todo
-  deleteTodo = (id) => {
-    const { todos } = this.state
-    const newTodos = todos.filter((todoObj, index) => {
-      return todoObj.id !== id
-    })
-    this.setState({todos: newTodos})
-  }
-
-  // 更新todo
-  updateTodo = (id, done) => {
-    const { todos } = this.state
-    const newTodos = todos.map((todoObj, index) => {
-      if (todoObj.id === id) {
-        todoObj.done = done
-      }
-      return todoObj
-    })
-    this.setState({todos: newTodos})
-  }
-
-  // 勾选或取消勾选
-  checkAll = (done) => {
-    const { todos } = this.state
-    const newTodos = todos.map(todoObj => ({...todoObj, done}))
-    this.setState({todos: newTodos})
-  }
-
-  // 清空已完成
-  deleteDone = () => {
-    const { todos } = this.state
-    const newTodos = todos.filter(todoObj => {
-      return !todoObj.done
-    })
-    this.setState({todos: newTodos})
+class App extends Component {
+  getStudents = () => {
+    const result = axios({
+      url: 'http://localhost:3000/students',
+      method: 'GET'
+    }).then(
+        response => {
+          console.log(response.data)
+        },
+        err => {
+          console.log(err)
+        }
+    )
+    console.log(result)
   }
 
   render() {
-    const { todos } = this.state
     return (
-      <div className="todo-wrap">
-        <Add addTodo={this.addTodo} />
-        <List todos={todos} deleteTodo={this.deleteTodo} updateTodo={this.updateTodo} />
-        <Footer todos={todos} checkAll={this.checkAll} deleteDone={this.deleteDone} />
-      </div>
-    )
+        <div>
+          <button onClick={this.getStudents}>点我获取学生信息</button>
+        </div>
+    );
   }
 }
 
+export default App;
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,10 +1,6 @@
-// 引入react核心库
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React  from 'react'
+import ReactDOM from 'react-dom'
 
-import './index.css'
-// 引入App根组件
-import App from './App';
+import App from './App'
 
-// <React.StrictMode>严格模式
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
