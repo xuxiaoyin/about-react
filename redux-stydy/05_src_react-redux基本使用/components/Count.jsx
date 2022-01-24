2517aff5cd9a48d50970a6d9da491d6a7a315f94
xiaoyin.xu@vcarsystem <1234567890>
2022-01-24 19:12:02 +0800

    react-redux优化一

---
+++ redux-stydy/05_src_react-redux基本使用/components/Count.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+class Count extends Component {
+  state = {  wind: '北风6级' }
+  // 加
+  increment = () => {
+    // 获取用户选择的数字
+    const { value } = this.numberNode
+    this.props.jia(value * 1)
+  }
+  // 减
+  decrement = () => {
+    // 获取用户选择的数字
+    const { value } = this.numberNode
+    this.props.jian(value * 1)
+  }
+  // 奇数加
+  incrementIfOdd = () => {
+    // 获取用户选择的数字
+    const { value } = this.numberNode
+    if (this.props.sum % 2 !== 0) {
+      this.props.jia(value * 1)
+    }
+  }
+  // 异步加
+  incrementAsync = () => {
+    // 获取用户选择的数字
+    const { value } = this.numberNode
+    this.props.jiaAsync(value * 1, 500)
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+        <div>
+          <h2>当前求和为：{this.props.sum}</h2>
+          <div>今天的天气：{this.state.wind}</div>
+          <select ref={c => this.numberNode = c}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>&nbsp;&nbsp;
+          <button onClick={this.increment}>+</button>&nbsp;&nbsp;
+          <button onClick={this.decrement}>-</button>&nbsp;&nbsp;
+          <button onClick={this.incrementIfOdd}>奇数再加</button>&nbsp;&nbsp;
+          <button onClick={this.incrementAsync}>异步加</button>
+        </div>
+    );
+  }
+}
+
+export default Count;
--- redux-stydy/src/container/Count/index.jsx
+++ redux-stydy/src/container/Count/index.jsx
@@ -1,31 +1,29 @@
-// 该文件是Count的容器组件，用于连接Count的UI 和 redux
+import CountUi from '../../components/Count'
+import { craeteIncrementAsyncAction, craeteDecrementAction, craeteIncrementAction} from '../../redux/count_action'
 
-// 引入count的ui组件
-import CountUI from '../../components/Count'
-import { craeteIncrementAction, craeteDecrementAction, craeteIncrementAsyncAction } from '../../redux/count_action'
-// 引入connect,用于：连接UI与redux，生成容器组件
 import { connect } from 'react-redux'
 
-// a函数的返回值必须是一个对象
-// 返回的这个对象的key就作为传递给UI组件的props的key
-// 返回的这个对象的value就作为传递给UI组件的props的value
-// a函数用于给UI组件传递状态
-function mapStateToProps(state) {
-  return { sum: state }
-}
+// 普通写法
+// const mapDispatchToProps = (dispatch) => (
+//     {
+//       jia: (value) => dispatch(craeteIncrementAction(value)),
+//       jian: (value) => dispatch(craeteDecrementAction(value)),
+//       jiaAsync: (value, time) => dispatch(craeteIncrementAsyncAction(value, time)),
+//     }
+// )
 
-// b函数的返回值必须是一个对象
-// 返回的这个对象的key就作为传递给UI组件的props的key
-// 返回的这个对象的value就作为传递给UI组件的props的value
-// b函数用于给UI组件传递操作状态的方法
-function mapDispatchToProps(dispatch) {
-  return {
-    jia: value => dispatch(craeteIncrementAction(value)),
-    jian: value => dispatch(craeteDecrementAction(value)),
-    jiaAsync: (value, time) => dispatch(craeteIncrementAsyncAction(value, time))
-  }
-}
+// 精简写法
+// const mapDispatchToProps = {
+//   jia: craeteIncrementAction,
+//   jian: craeteDecrementAction,
+//   jiaAsync: craeteIncrementAsyncAction,
+// }
 
-const CountContainer = connect(mapStateToProps, mapDispatchToProps)(CountUI)
-
-export default CountContainer
+export default connect(
+    (state) => ({sum: state}), // 映射状态
+    {
+      jia: craeteIncrementAction,
+      jian: craeteDecrementAction,
+      jiaAsync: craeteIncrementAsyncAction,
+    } // 映射状态操作的方法
+)(CountUi)
