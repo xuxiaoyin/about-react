06532ec72cbc6d46a4788c8675b10120eff3843f
xiaoyin.xu@vcarsystem <1234567890>
2022-01-25 15:11:54 +0800

    路由懒加载

--- new-project/src/components/2_lazyLoad/index.jsx
+++ new-project/src/components/2_lazyLoad/index.jsx
@@ -1,7 +1,10 @@
-import React, {Component} from 'react';
-import Home from './Home';
-import About from './About';
+import React, {Component, lazy, Suspense} from 'react';
+// import Home from './Home';
+// import About from './About';
+import Loading from './Loading';
 import {Routes, Route, NavLink } from 'react-router-dom'
+const Home = lazy(() => import('./Home'))
+const About = lazy(() => import('./About'))
 
 class Demo extends Component {
   render() {
@@ -9,9 +12,12 @@ class Demo extends Component {
         <div>
           <NavLink to="/home">Home</NavLink>
           <NavLink to="/about">About</NavLink>
-          <Routes>
-            <Route element={<Home />}></Route>
-          </Routes>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/home" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+            </Routes>
+          </Suspense>
         </div>
     );
   }
